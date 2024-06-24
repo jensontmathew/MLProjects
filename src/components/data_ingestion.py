e12f09f2752ea3dc37b7d406171612129a96b479
Jenson T Mathew <jensontmathew@Jensons-MacBook-Air.local>
2024-06-24 22:09:14 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,6 +4,9 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass #used to create class variables
 
@@ -40,8 +43,16 @@ class DataIngestion:
         except:
             raise CustomException(error_message="Error occured while reading the data",error_detail=sys.exc_info())
         
-if __name__== "__main__":
+# if __name__== "__main__":
+#     obj=DataIngestion()
+#     train_data,test_data=obj.initiate_data_ingestion()
+
+#     data_transformation=DataTransformation()
+#     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
 
-
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
